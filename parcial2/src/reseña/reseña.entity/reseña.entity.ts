368e535906670896a18f23e7f3f7c77922465fa3
Juan Miguel Manrique <j.manriquec@uniandes.edu.co>
2025-05-16 09:20:41 -0500

    dos

--- parcial2/src/reseña/reseña.entity/reseña.entity.ts
+++ parcial2/src/reseña/reseña.entity/reseña.entity.ts
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+//1
 import { ActividadEntity } from "src/actividad/actividad.entity/actividad.entity";
 import { EstudianteEntity } from "src/estudiante/estudiante.entity/estudiante.entity";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
